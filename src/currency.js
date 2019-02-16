74c4480d1e26c619090e2ec88926d4d4f3c60d85
yasin <yasin.junet@gmail.com>
2019-02-16 10:07:17 +0700

    add currency description

---
+++ src/currency.js
@@ -0,0 +1,18 @@
+/*
+ * I made this because there is no currencies description on end-point
+ */
+
+const currency = {
+  USD: "United States Dollar",
+  CAD: "Canadian Dollar",
+  IDR: "Indonesian Rupiah",
+  GBP: "Pound Sterling",
+  CHF: "Swiss Franc",
+  SGD: "Singapore Dollar",
+  INR: "Indian Rupee",
+  MYR: "Malaysian Ringgit",
+  JPY: "Japanese Yen",
+  KRW: "South Korean Won"
+};
+
+export default currency;
