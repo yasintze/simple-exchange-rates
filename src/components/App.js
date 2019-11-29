dd84f50f707909e1f1907f86483a48b59a035c1c
yasin <yasintze@gmail.com>
2019-11-29 14:19:00 +0700

    Remove redux, use hooks for rematch

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 // @flow
-import React from "react";
+import React, { useState, useEffect } from "react";
 import compose from "recompose/compose";
-import { connect } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import Grid from "@material-ui/core/Grid";
 import Paper from "@material-ui/core/Paper";
 import lightGreen from "@material-ui/core/colors/lightGreen";
@@ -16,20 +16,16 @@ import Currency from "./Currency";
 import AddCurrency from "./AddCurrency";
 
 type Props = {
-  feed: Array<Object>,
-  date: string,
-  fetchFeedAsync: Function,
-  loading: boolean,
   classes: Object
 };
 
-type State = {
-  data: Array<Object>,
-  selected: string,
-  input: string,
-  btnDisable: boolean,
-  count: number
-};
+// type State = {
+//   data: Array<Object>,
+//   selected: string,
+//   input: string,
+//   btnDisable: boolean,
+//   count: number
+// };
 
 const styles = () => ({
   deviceHeight: {
@@ -46,22 +42,21 @@ const styles = () => ({
   }
 });
 
-class App extends React.Component<Props, State> {
-  constructor(props: Props) {
-    super(props);
+const App = (props: Props) => {
+  const { classes } = props;
+  const feed = useSelector(state => state.feed.result);
+  const date = useSelector(state => state.feed.date);
+  const loading = useSelector(state => state.loading.models.feed);
+  const dispatch = useDispatch();
 
-    this.state = {
-      data: [],
-      selected: "",
-      input: "10",
-      btnDisable: true,
-      count: 0
-    };
-  }
+  const [data, setData] = useState([]);
+  const [selected, setSelected] = useState("");
+  const [input, setInput] = useState("10");
+  const [btnDisable, setBtnDisable] = useState(true);
+  const [count, setCount] = useState(0);
 
-  componentDidMount() {
+  useEffect(() => {
     const element = document.getElementById("initLoader");
-    const { fetchFeedAsync } = this.props;
     window.onload = () => {
       if (element) {
         element.remove();
@@ -69,128 +64,85 @@ class App extends React.Component<Props, State> {
     };
 
     // Fetch rates data
-    fetchFeedAsync();
-  }
+    dispatch.feed.fetchFeedAsync();
+  }, []);
 
-  handleInputFocus(value: string) {
-    this.setState({
-      input: numeral(value).value()
-    });
-  }
+  const handleInputFocus = value => {
+    setInput(value);
+  };
 
-  handleInputBlur(value: string) {
-    this.setState({
-      input: numeral(value).format("0,0.00")
-    });
-  }
+  const handleInputBlur = value => {
+    setInput(numeral(value).format("0,0.00"));
+  };
 
-  handleInputChange(value: string) {
-    this.setState({
-      input: value
-    });
-  }
+  const handleInputChange = value => {
+    setInput(value);
+  };
 
-  handleSelectChange(value: string) {
-    this.setState({
-      selected: value,
-      btnDisable: value === ""
-    });
-  }
+  const handleSelectChange = value => {
+    setSelected(value);
+    setBtnDisable(value === "");
+  };
 
-  handleClickButton() {
-    const { feed } = this.props;
-    const { data, selected, count } = this.state;
+  const handleClickButton = () => {
     const newData = {
       id: count,
       name: selected,
       detail: currency[selected],
       rate: feed[selected]
     };
-    this.setState({
-      data: [...data, newData],
-      selected: "",
-      btnDisable: true,
-      count: count + 1
-    });
-  }
+    setData([...data, newData]);
+    setSelected("");
+    setBtnDisable(true);
+    setCount(count + 1);
+  };
 
-  handleRemoveCurrency(id: number) {
-    const { data, count } = this.state;
+  const handleRemoveCurrency = id => {
     const arr = data.filter(el => {
       return el.id !== id;
     });
-    this.setState({
-      data: arr,
-      count: count - 1
-    });
-  }
+    setData(arr);
+    setCount(count - 1);
+  };
 
-  render() {
-    const { feed, date, loading, classes } = this.props;
-    const { data, selected, input, btnDisable } = this.state;
-
-    return (
-      <div className={classes.deviceHeight}>
-        <Header
-          input={input}
-          onFocus={e => this.handleInputFocus(e.target.value)}
-          onBlur={e => this.handleInputBlur(e.target.value)}
-          onChange={e => this.handleInputChange(e.target.value)}
-        />
-        <div className={classes.root}>
-          <Grid container>
-            <Grid item xs>
-              <Paper className={classes.lightGreenPaper}>
-                {loading.modal && <Loader />}
-                {data.map((item, id) => (
-                  <Currency
-                    key={item.name}
-                    id={id}
-                    name={item.name}
-                    detail={item.detail}
-                    rate={item.rate}
-                    value={input}
-                    removeCurrency={() => this.handleRemoveCurrency(id)}
-                  />
-                ))}
-              </Paper>
-              <AddCurrency
-                data={feed}
-                selected={selected}
-                button={btnDisable}
-                onChange={e => this.handleSelectChange(e.target.value)}
-                onClick={() => this.handleClickButton()}
-              />
-            </Grid>
+  return (
+    <div className={classes.deviceHeight}>
+      <Header
+        input={input}
+        onFocus={e => handleInputFocus(e.target.value)}
+        onBlur={e => handleInputBlur(e.target.value)}
+        onChange={e => handleInputChange(e.target.value)}
+      />
+      <div className={classes.root}>
+        <Grid container>
+          <Grid item xs>
+            <Paper className={classes.lightGreenPaper}>
+              {loading.modal && <Loader />}
+              {data.map((item, id) => (
+                <Currency
+                  key={item.name}
+                  id={id}
+                  name={item.name}
+                  detail={item.detail}
+                  rate={item.rate}
+                  value={input}
+                  removeCurrency={() => handleRemoveCurrency(id)}
+                />
+              ))}
+            </Paper>
+            <AddCurrency
+              data={feed}
+              selected={selected}
+              button={btnDisable}
+              onChange={e => handleSelectChange(e.target.value)}
+              onClick={() => handleClickButton()}
+            />
           </Grid>
-          <Footer date={date} />
-        </div>
+        </Grid>
+        <Footer date={date} />
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
-function mapStateToProps(state) {
-  return {
-    feed: state.feed.result,
-    date: state.feed.date,
-    loading: {
-      global: state.loading.global,
-      model: state.loading.models.feed
-    }
-  };
-}
-
-function mapDispatchToProps(dispatch) {
-  return {
-    fetchFeedAsync: dispatch.feed.fetchFeedAsync
-  };
-}
-
-export default compose(
-  withStyles(styles),
-  connect(
-    mapStateToProps,
-    mapDispatchToProps
-  )
-)(App);
+export default compose(withStyles(styles))(App);
